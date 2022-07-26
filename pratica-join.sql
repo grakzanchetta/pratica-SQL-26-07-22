f611fbf670c567de3c36993015de9a909d7df175
Gabriel Rak <grakzanchetta@gmail.com>
2022-07-26 19:27:00 -0300

    Terceiro exercicio

--- pratica-join.sql
+++ pratica-join.sql
@@ -6,6 +6,15 @@ FROM users
 JOIN cities ON users."cityId" = cities.id 
 WHERE cities.name = 'Rio de Janeiro';
 
+--Exercício 3
+
+SELECT users.id, users.name, courses.name, schools.name, educations."endDate"
+FROM educations
+JOIN users ON educations."userId" = users.id
+JOIN courses ON educations."courseId" = courses.id
+JOIN schools ON educations."schoolId" = schools.id
+WHERE users.id = 30;
+
 
 
 
---
+++ pratica-join.sql~
@@ -0,0 +1,13 @@
+-- Praticas com Join dia 26/06/2022
+
+--Exercicio 1
+SELECT users.id, users.name, cities.name 
+FROM users 
+JOIN cities ON users."cityId" = cities.id 
+WHERE cities.name = 'Rio de Janeiro';
+
+
+
+
+
+
